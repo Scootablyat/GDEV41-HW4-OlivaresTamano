dbc1db06fd47571fc63e44e978848af74b4e44a3
csci40 finalproject <christianolivares13@gmail.com>
2024-10-29 22:44:02 +0800

    fixed list problem in vectors

--- Main.cpp
+++ Main.cpp
@@ -13,7 +13,7 @@ const int WINDOW_HEIGHT = 720;
 const float FPS = 60;
 const float TIMESTEP = 1 / FPS;
 const int cellSize = 50;
-const int MAX_DEPTH = 7;
+const int MAX_DEPTH = 4;
 
 struct Node;
 struct Ball;
@@ -33,7 +33,7 @@ struct Node
     // Removes the specific ball from the node
     void RemoveBall(Ball *ball)
     {
-        for (size_t i = 0; i < balls_in_node.size(); i++)
+        for (int i = 0; i < balls_in_node.size(); i++)
         {
             if (balls_in_node[i] == ball)
             {
@@ -128,10 +128,22 @@ void InitializeNodes(Node *parent_node, int current_depth)
     {
         parent_node->isLeaf = true;
     }
+}
 
-    // std::cout << "Grid " << current_depth << parent_node->depth << ". " << "Center: {" << parent_node->position.x << ", " << parent_node->position.y << "}. ";
-    // std::cout << "Half: " << parent_node->half_width << ". Min: {" << parent_node->min.x << ", " << parent_node->min.y << "}. Max: {" << parent_node->max.x << ", " << parent_node->max.y << "}.";
-    // std::cout << "Leaf: " << parent_node->isLeaf << std::endl;
+void UpdateNodes(Node *current_node, int current_depth)
+{
+    if (!current_node->balls_in_node.empty())
+    {
+        current_node->balls_in_node.clear();
+    }
+
+    if (current_depth < MAX_DEPTH) // From root node to MAX_DEPTH-1
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            UpdateNodes(current_node->child[i], current_depth + 1);
+        }
+    }
 }
 
 void InsertBallToNode(Ball *ball, Node *node)
@@ -159,6 +171,8 @@ void DrawQuadTree(Node *current_node)
     if (!current_node->balls_in_node.empty())
     {
         // std::cout << "Node depth " << current_node->depth << "has" << current_node->balls_in_node.size() << "balls" << std::endl;
+        const char *numberOfBalsInCell = std::to_string(current_node->balls_in_node.size()).c_str();
+        DrawText(numberOfBalsInCell, current_node->max.x - current_node->half_width * 1.05, current_node->max.y - current_node->half_width * 1.15, 20, PURPLE);
         DrawRectangleLines(current_node->min.x, current_node->min.y, current_node->half_width * 2, current_node->half_width * 2, BLACK);
     }
     if (!current_node->isLeaf)
@@ -188,7 +202,10 @@ void UpdateBall(Ball *ball)
                 ball->current_node->child[i]->min.x <= ball->position.x - ball->radius &&
                 ball->current_node->child[i]->min.y <= ball->position.y - ball->radius)
             {
-                InsertBallToNode(ball, ball->current_node->child[i]); // Start going down again
+                // ball->current_node->balls_in_node.erase(std::remove(ball->current_node->balls_in_node.begin(), ball->current_node->balls_in_node.end(), ball), ball->current_node->balls_in_node.end());
+                ball->current_node->RemoveBall(ball);
+                ball->current_node = ball->current_node->child[i];
+                InsertBallToNode(ball, ball->current_node); // Start going down again
             }
         }
     }
@@ -196,8 +213,8 @@ void UpdateBall(Ball *ball)
     {
         // Sauce: https://stackoverflow.com/questions/41946007/efficient-and-well-explained-implementation-of-a-quadtree-for-2d-collision-det
         // Erases the node's reference to this particular ball
-        ball->current_node->RemoveBall(ball);
-        // ball->current_node->balls_in_node.erase(std::remove(ball->current_node->balls_in_node.begin(), ball->current_node->balls_in_node.end(), ball), ball->current_node->balls_in_node.end());
+        // ball->current_node->RemoveBall(ball);
+        ball->current_node->balls_in_node.erase(std::remove(ball->current_node->balls_in_node.begin(), ball->current_node->balls_in_node.end(), ball), ball->current_node->balls_in_node.end());
 
         // Changing the ball's reference to the former node's parent. Keep going up until we find an appropriate node
         ball->current_node = ball->current_node->parent;
@@ -230,244 +247,13 @@ void CheckBallCollision(Ball *ball, Node *current_node)
     {
         for (int i = 0; i < 4; i++)
         {
-            if (!current_node->child[i]->balls_in_node.empty()) // Checking if there's balls
-            {
-                CheckBallCollision(ball, current_node->child[i]);
-            }
+            // if (!current_node->child[i]->balls_in_node.empty()) // Checking if there's balls
+            // {
+            CheckBallCollision(ball, current_node->child[i]);
+            // }
         }
     }
 };
-
-struct cell
-{
-    Vector2 position = {0.0f, 0.0f};
-    Color color = RED;
-    Vector2 max;
-    Vector2 min;
-
-    std::vector<Ball> ballsInCell;
-
-    bool operator==(const cell &cell)
-    {
-        return (this->position.x == cell.position.x && this->position.y == cell.position.y);
-    }
-
-    bool operator==(const Vector2 &position)
-    {
-        return (this->position.x == position.x && this->position.y == position.y);
-    }
-    void addBall(Ball &ball)
-    {
-        if (std::find(ballsInCell.begin(), ballsInCell.end(), ball) != ballsInCell.end())
-        {
-        }
-        else
-        {
-            this->ballsInCell.push_back(ball);
-        }
-    }
-    void clearBalls()
-    {
-        this->ballsInCell.clear();
-    }
-
-    bool isEmpty()
-    {
-        return (this->ballsInCell.size() <= 0);
-    }
-};
-
-float getDistance(Ball b1, Ball b2)
-{
-    Vector2 dist = Vector2Subtract(b1.position, b2.position);
-    return std::abs(Vector2Length(dist));
-}
-
-float getDistanceToPoint(Ball b1, Vector2 pos)
-{
-    Vector2 dist = Vector2Subtract(b1.position, pos);
-    return std::abs(Vector2Length(dist));
-}
-
-bool isCirclesColliding(Ball b1, Ball b2)
-{
-    float sumOfRadii = b1.radius + b2.radius;
-    float distance = getDistance(b1, b2);
-    if (distance <= sumOfRadii)
-    {
-        return true;
-    }
-    return false;
-}
-
-Vector2 getNearestIndexAtPoint(Vector2 position)
-{ // get the index of the cell (inverted)
-    if (position.x < 0)
-    {
-        return Vector2{0, std::floor(position.y / cellSize)};
-    }
-    if (position.y < 0)
-    {
-        return Vector2{std::floor(position.x / cellSize), 0};
-    }
-    if (position.x < 0 && position.y < 0)
-    {
-        return Vector2{0, 0};
-    }
-
-    if (position.x > WINDOW_WIDTH)
-    {
-        return Vector2{std::floor((float)WINDOW_WIDTH / cellSize), std::floor(position.y / cellSize)};
-    }
-    if (position.y > WINDOW_HEIGHT)
-    {
-        return Vector2{std::floor(position.x / cellSize), std::floor((float)WINDOW_HEIGHT / cellSize)};
-    }
-    if (position.x > WINDOW_WIDTH && position.y > WINDOW_HEIGHT)
-    {
-        return Vector2{std::floor((float)WINDOW_WIDTH / cellSize), std::floor((float)WINDOW_HEIGHT / cellSize)};
-    }
-    return Vector2{std::floor(position.x / cellSize), std::floor(position.y / cellSize)};
-}
-
-void initializeCell(cell &testCell, Vector2 pos, Color color)
-{
-    testCell.position = pos;
-    testCell.max = Vector2{testCell.position.x + cellSize, testCell.position.y};
-    testCell.min = Vector2{testCell.position.x, testCell.position.y + cellSize};
-    testCell.color = color;
-}
-
-void initializeAllCells(std::vector<std::vector<cell>> &Cells)
-{
-    // get array of columns, store it in a row.
-    int numberOFRows = std::ceil((float)WINDOW_HEIGHT / (float)cellSize);
-    int numberOfColumns = std::ceil((float)WINDOW_WIDTH / (float)cellSize);
-    std::cout << numberOFRows << std::endl;
-    std::cout << numberOfColumns << std::endl;
-    for (int i = 0; i < numberOFRows; i++)
-    {
-        std::vector<cell> row;
-        for (int j = 0; j < numberOfColumns; j++)
-        {
-            cell x;
-            initializeCell(x, Vector2{(float)j * cellSize, (float)i * cellSize}, RED);
-            row.push_back(x);
-        }
-        Cells.push_back(row);
-    }
-}
-
-void addBallToCell(std::vector<std::vector<cell>> &grid, Ball ball)
-{
-    Vector2 max = Vector2{ball.position.x + ball.radius, ball.position.y + ball.radius};
-    Vector2 min = Vector2{ball.position.x - ball.radius, ball.position.y - ball.radius};
-
-    Vector2 indexAtMin = getNearestIndexAtPoint(min);
-    Vector2 indexAtCenter = getNearestIndexAtPoint(ball.position);
-    Vector2 indexAtMax = getNearestIndexAtPoint(max);
-    Vector2 indexAtLowerRight = getNearestIndexAtPoint(Vector2{max.x, min.y});
-    Vector2 indexAtUpperLeft = getNearestIndexAtPoint(Vector2{min.x, max.y});
-
-    // std::cout << "Index At center" << indexAtCenter.x << " " << indexAtCenter.y << std::endl;
-    grid[indexAtCenter.y][indexAtCenter.x].addBall(ball);
-    grid[indexAtCenter.y][indexAtCenter.x].color = BLUE;
-
-    grid[indexAtMin.y][indexAtMin.x].addBall(ball);
-    grid[indexAtMin.y][indexAtMin.x].color = BLUE;
-
-    grid[indexAtMax.y][indexAtMax.x].addBall(ball);
-    grid[indexAtMax.y][indexAtMax.x].color = BLUE;
-
-    grid[indexAtLowerRight.y][indexAtLowerRight.x].addBall(ball);
-    grid[indexAtLowerRight.y][indexAtLowerRight.x].color = BLUE;
-
-    grid[indexAtUpperLeft.y][indexAtUpperLeft.x].addBall(ball);
-    grid[indexAtUpperLeft.y][indexAtUpperLeft.x].color = BLUE;
-}
-
-void updateCellContents(std::vector<std::vector<cell>> &grid, std::vector<Ball> &balls)
-{
-    for (int i = 0; i < grid.size(); i++)
-    {
-        for (int j = 0; j < grid[i].size(); j++)
-        {
-            grid[i][j].clearBalls();
-            if (grid[i][j].ballsInCell.size() > 0)
-            {
-                grid[i][j].color = BLUE;
-            }
-            else
-            {
-                grid[i][j].color = RED;
-            }
-        }
-    }
-    for (int k = 0; k < balls.size(); k++)
-    {
-        addBallToCell(grid, balls[k]);
-    }
-}
-
-void checkCollisionInCell(std::vector<std::vector<cell>> &grid, float elasticityCoefficient, std::vector<Ball> &ballArray)
-{
-    for (int i = 0; i < grid.size(); i++)
-    {
-        for (int j = 0; j < grid[i].size(); j++)
-        {
-            for (int k = 0; k < grid[i][j].ballsInCell.size(); k++)
-            {
-
-                ballArray[grid[i][j].ballsInCell[k].index].position = Vector2Add(grid[i][j].ballsInCell[k].position, Vector2Scale(grid[i][j].ballsInCell[k].velocity, TIMESTEP));
-
-                if (ballArray[grid[i][j].ballsInCell[k].index].position.x - ballArray[grid[i][j].ballsInCell[k].index].radius <= 0)
-                {
-                    ballArray[grid[i][j].ballsInCell[k].index].position.x = ballArray[grid[i][j].ballsInCell[k].index].radius;
-                    ballArray[grid[i][j].ballsInCell[k].index].velocity.x *= -1;
-                }
-                if (ballArray[grid[i][j].ballsInCell[k].index].position.x + ballArray[grid[i][j].ballsInCell[k].index].radius >= WINDOW_WIDTH)
-                {
-                    ballArray[grid[i][j].ballsInCell[k].index].position.x = WINDOW_WIDTH - ballArray[grid[i][j].ballsInCell[k].index].radius;
-                    ballArray[grid[i][j].ballsInCell[k].index].velocity.x *= -1;
-                }
-                if (ballArray[grid[i][j].ballsInCell[k].index].position.y - ballArray[grid[i][j].ballsInCell[k].index].radius <= 0)
-                {
-                    ballArray[grid[i][j].ballsInCell[k].index].position.y = ballArray[grid[i][j].ballsInCell[k].index].radius;
-                    ballArray[grid[i][j].ballsInCell[k].index].velocity.y *= -1;
-                }
-                if (ballArray[grid[i][j].ballsInCell[k].index].position.y + ballArray[grid[i][j].ballsInCell[k].index].radius >= WINDOW_HEIGHT)
-                {
-                    ballArray[grid[i][j].ballsInCell[k].index].position.y = WINDOW_HEIGHT - ballArray[grid[i][j].ballsInCell[k].index].radius;
-                    ballArray[grid[i][j].ballsInCell[k].index].velocity.y *= -1;
-                }
-
-                for (int l = 0; l < grid[i][j].ballsInCell.size(); l++)
-                {
-                    if (l == k)
-                    {
-                        continue;
-                    }
-                    else if (l >= grid[i][j].ballsInCell.size() || k >= grid[i][j].ballsInCell.size())
-                    {
-                        break;
-                    }
-                    Vector2 n = Vector2Normalize(Vector2Subtract(ballArray[grid[i][j].ballsInCell[k].index].position, ballArray[grid[i][j].ballsInCell[l].index].position));
-                    if (isCirclesColliding(ballArray[grid[i][j].ballsInCell[k].index], ballArray[grid[i][j].ballsInCell[l].index]) && Vector2DotProduct(n, Vector2Subtract(grid[i][j].ballsInCell[k].velocity, grid[i][j].ballsInCell[l].velocity)) < 0)
-                    {
-                        float impulsej = -((
-                                               (1 + elasticityCoefficient) * Vector2DotProduct(Vector2Subtract(ballArray[grid[i][j].ballsInCell[k].index].velocity, ballArray[grid[i][j].ballsInCell[l].index].velocity), n)) /
-                                           (Vector2DotProduct(n, n) * (1 / ballArray[grid[i][j].ballsInCell[k].index].mass) + (1 / ballArray[grid[i][j].ballsInCell[l].index].mass)));
-
-                        Vector2 newVelocity = Vector2Add(ballArray[grid[i][j].ballsInCell[k].index].velocity, Vector2Scale(n, (impulsej / ballArray[grid[i][j].ballsInCell[k].index].mass)));
-                        ballArray[grid[i][j].ballsInCell[k].index].velocity = newVelocity;
-                        Vector2 newVelocity2 = Vector2Subtract(ballArray[grid[i][j].ballsInCell[l].index].velocity, Vector2Scale(n, (impulsej / ballArray[grid[i][j].ballsInCell[l].index].mass)));
-                        ballArray[grid[i][j].ballsInCell[l].index].velocity = newVelocity2;
-                    }
-                }
-            }
-        }
-    }
-}
 
 float RandomDirection()
 {
@@ -502,7 +288,7 @@ void InitializeBall(std::vector<Ball> &array, int arraySize, bool isLarge, Node 
             ball.inverse_mass = 1.0f;
         }
         ball.color = randomColor;
-        ball.velocity = {500.0f * RandomDirection(), 500.0f * RandomDirection()};
+        ball.velocity = {250.0f * RandomDirection(), 250.0f * RandomDirection()};
         // ball.index = index;
         // index++;
         array.push_back(ball);
@@ -510,19 +296,6 @@ void InitializeBall(std::vector<Ball> &array, int arraySize, bool isLarge, Node 
         InsertBallToNode(&array.back(), node); // BALL INSERTION. Starting with root node as "current node". Balls recursively move down the tree
         // std::cout << "node # " << array.size() << " at quad " << array.back().current_node->position.x << ", " << array.back().current_node->position.y << std::endl;
     }
-}
-
-void updateBallsIndex(std::vector<Ball> &ballArray)
-{
-    for (int i = 0; i < ballArray.size(); i++)
-    {
-        ballArray[i].index = i;
-    }
-}
-
-Vector2 getCenterOfRectangle(Vector2 RectanglePos, float width, float height)
-{ // ( (x1 + x2) / 2, (y1 + y2) / 2 )
-    return Vector2{(RectanglePos.x + width) / 2, (RectanglePos.y + height) / 2};
 }
 
 int main()
@@ -536,6 +309,7 @@ int main()
     float accumulator = 0;
 
     std::vector<Ball> ballArray;
+    ballArray.reserve(10000);
     int spawnInstance = 0;
 
     // initialize Cell
@@ -552,7 +326,8 @@ int main()
 
     while (!WindowShouldClose())
     {
-
+        // UpdateNodes(&root_node, 0); // recreate the quadtree and reinsert all the objects with their updated positions per frame
+        DrawFPS(WINDOW_WIDTH - 75, 10);
         float delta_time = GetFrameTime();
         Vector2 forces = Vector2Zero();
 
@@ -609,7 +384,8 @@ int main()
                     current_ball.velocity.y *= -1.0f;
                 }
 
-                UpdateBall(&current_ball);
+                UpdateBall(&current_ball); // Updates the ball's current node
+
                 // InsertBallToNode(&current_ball, &root_node); // Update ball
                 CheckBallCollision(&current_ball, current_ball.current_node);
             }
@@ -617,8 +393,8 @@ int main()
             // checkCollisionInCell(grid, elasticityCoefficient, ballArray);
             accumulator -= TIMESTEP;
         }
-        const char *numberOfBalls = std::to_string(ballArray.size()).c_str();
 
+        const char *numberOfBalls = std::to_string(ballArray.size()).c_str();
         BeginDrawing();
         ClearBackground(WHITE);
         DrawText(numberOfBalls, 0, 0, 30, YELLOW);
@@ -626,21 +402,6 @@ int main()
         {
             DrawCircleV(ballArray[i].position, ballArray[i].radius, ballArray[i].color);
         }
-
-        // if (drawGrid)
-        // {
-        //     for (int i = 0; i < grid.size(); i++)
-        //     {
-        //         for (int j = 0; j < grid[i].size(); j++)
-        //         {
-        //             const char *numberOfBalsInCell = std::to_string(grid[i][j].ballsInCell.size()).c_str();
-        //             Vector2 rectMidpoint = Vector2{getCenterOfRectangle(grid[i][j].position, cellSize, cellSize)};
-        //             DrawText(numberOfBalsInCell, grid[i][j].max.x, grid[i][j].max.y, 5, PURPLE);
-        //             DrawRectangleLines(grid[i][j].position.x, grid[i][j].position.y, cellSize, cellSize, grid[i][j].color);
-        //         }
-        //     }
-        // }
-
         // if (drawGrid)
         // {
         DrawQuadTree(&root_node);
